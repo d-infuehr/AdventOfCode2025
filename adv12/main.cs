4829ba3afd9a9f6a6208b8ca4b457ea5df094fb3
Daniel Infuehr <daniel.infuehr@outlook.com>
2025-12-12 14:21:37 +0100

    AOC day 12

---
+++ adv12/main.cs
@@ -0,0 +1,22 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+List<int> areas = new List<int>();
+
+int works = 0;
+for (int i = 0; i < lines.Length; i++) {
+    if (lines[i].Contains('x')) {
+        int area = lines[i].Split(':')[0].Split('x').Select(int.Parse).Aggregate((a, b) => a * b);
+        int[] objCounts = lines[i].Split(':')[1].Trim().Split(' ').Select(int.Parse).ToArray();
+        int objArea = 0;
+        for (int n = 0; n < objCounts.Length; n++) {
+            objArea += objCounts[n] * areas[n];
+        }
+        works += objArea <= area ? 1 : 0;
+    } else {
+        string shape = "";
+        while (lines[i] != "") {
+            shape += lines[i++];
+        }
+        areas.Add(shape.Count(x => x == '#'));
+    }
+}
+Console.WriteLine(works);
